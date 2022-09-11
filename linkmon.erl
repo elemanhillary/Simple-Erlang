9e0b7c148c6897f1999f54cf809fdff263967925
elemanhillary <elemanhillary@gmail.com>
2022-09-11 14:50:05 +0300

    - monitoring, links, and recovery

---
+++ linkmon.erl
@@ -0,0 +1,41 @@
+-module(linkmon).
+-compile(export_all).
+
+start_critic1() -> spawn(?MODULE, restart, []).
+start_critic() -> spawn(?MODULE, critic, []).
+
+restart() ->
+    process_flag(trap_exit, true),
+    Pid = spawn_link(?MODULE, critic, []),
+    register(critic, Pid),
+    receive
+        {'EXIT', Pid, normal} ->
+            ok;
+        {'EXIT', Pid, shutdown} ->
+            ok;
+        {'EXIT', Pid, _} ->
+            restart()
+    end.
+
+judge(Band, Album) ->
+    Ref = make_ref(),
+    critic ! {self(), Ref, {Band, Album}},
+    receive
+        {Ref, Criticism} -> Criticism
+    after 2000 ->
+        timeout
+    end.
+
+
+critic() -> 
+    receive
+        {From, Ref, {"Rage Against the Turing Machine", "Unit Testify"}} ->
+            From ! {Ref, "They are great!"};
+        {From, Ref, {"System of a Downtime", "Memoize"}} ->
+            From ! {Ref, "They're not Johnny Crash but they're good."};
+        {From, Ref, {"Johnny Crash", "The Token Ring of Fire"}} ->
+            From ! {Ref, "Simply incredible."};
+        {From, Ref, {_Band, _Album}} ->
+            From ! {Ref, "They are terrible!"}
+    end,
+    critic().
