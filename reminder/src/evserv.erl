d358552db300f9607465dacc124874b4783a208d
kliques <elemanhillary@gmail.com>
2022-09-25 13:04:24 +0300

    - erlang todo list with events

--- reminder/src/evserv.erl
+++ reminder/src/evserv.erl
@@ -1,7 +1,7 @@
 -module(evserv).
 -compile(export_all).
 -record(state, {
-    event,
+    events,
     clients
     }).
 -record(event, {
@@ -12,7 +12,7 @@
     }).
 
 init() ->
-    loop(#state{event=orddict:new(), clients=orddict:new()}).
+    loop(#state{events=orddict:new(), clients=orddict:new()}).
 
 loop(S = #state{}) ->
     receive
@@ -22,12 +22,66 @@ loop(S = #state{}) ->
             Pid ! {MsgRef, ok},
             loop(S#state{clients=NewClient});
         {Pid, MsgRef, {add, Name, Description, TimeOut}} ->
+            case valid_datetime(TimeOut) of
+                true ->
+                    EventPid = event:start_link(Name, TimeOut),
+                    NewEvent = orddict:store(Name,#event{
+                        name = Name,
+                        description = Description,
+                        pid = EventPid,
+                        timeout = TimeOut
+                        }, S#state.events),
+                    Pid ! {MsgRef, ok},
+                    loop((S#state{events=NewEvent}));
+                false ->
+                    Pid ! {MsgRef, {error, bad_timeout}},
+                    loop(S)
+            end;
         {Pid, MsgRef, {cancel, Name}} ->
+            Event = case orddict:find(Name, S#event.name) of
+                {ok, E} ->
+                    event:cancel(E#event.pid),
+                    orddict:erase(Name, S#state.events);
+                error ->
+                    S#state.events
+            end,
+            Pid ! {MsgRef, ok},
+            loop(S#state{events=Event});
         {done, Name} ->
+            case orddict:find(Name, S#state.events) of
+                {ok, E} ->
+                    send_to_clients({done, E#event.name, E#event.description}, S#state.clients),
+                    NewEvent = orddict:erase(Name, S#state.events),
+                    loop(S#state{events=NewEvent});
+                error ->
+                    loop(S)
+            end;
         shutdown ->
+            exit(shutdown);
         {'DOWN', Ref, process, _Pid, _Reason} ->
+            loop(S#state{clients=orddict:erase(Ref, S#state.clients)});
         code_change ->
+            ?MODULE:loop(S);
         Unknown ->
             io:format("Unknown message: ~p~n",[Unknown]),
             loop(S)
     end.
+
+send_to_clients(Msg, ClientDict) ->
+    orddict:map(fun(_Ref, Pid) -> Pid ! Msg end, ClientDict).
+
+valid_datetime({Date, Time}) ->
+    try
+        calendar:valid_date(Date) andalso valid_time(Time)
+    catch
+        error:function_clause ->
+            false
+    end.
+
+
+valid_time({H, M, S}) -> valid_time(H, M, S).
+valid_time(H, M, S) when H >= 0, H < 24,
+                         M >= 0, M < 60,
+                         S >= 0, S < 60 ->
+                            true;
+valid_time(_,_,_) -> false.
